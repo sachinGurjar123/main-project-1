3f15a36ffce6132a38cc9c3fd19e346986be2e79
sachingit <120475140+sachinGurjar123@users.noreply.github.com>
2023-07-26 01:53:07 +0530

    Update test

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>hello developers</h1>;
+  return (
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti ducimus
+      laudantium unde a nulla placeat quos. Quos eum atque aliquam delectus vero
+      itaque nam, vel sequi culpa molestias tenetur inventore!
+    </p>
+  );
 }
