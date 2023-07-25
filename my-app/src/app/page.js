432a3125db2e6fe3153f4366084c533f0616f1cb
skumargurjar2023 <138102902+skumargurjar2023@users.noreply.github.com>
2023-07-26 01:56:14 +0530

    Dev (#1)
    * Upload project
    
    * Update test
    
    ---------
    
    Co-authored-by: sachingit <120475140+sachinGurjar123@users.noreply.github.com>

---
+++ my-app/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti ducimus
+      laudantium unde a nulla placeat quos. Quos eum atque aliquam delectus vero
+      itaque nam, vel sequi culpa molestias tenetur inventore!
+    </p>
+  );
+}
